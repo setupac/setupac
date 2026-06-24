c9e7f4749addaf534157924d201242d38e1119a8
Ajay Chaudhary <ajayver110125@gmail.com>
2026-06-25 00:43:08 +0530

    initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# Setupac
+
+⚠️ Setupac is currently under development.
+
+The package name has been reserved and functionality will be added in future releases.
+
+Current version: 0.0.1
---
+++ bin/setupac.js
@@ -0,0 +1,6 @@
+#!/usr/bin/env node
+
+console.log("Setupac v0.0.1");
+console.log("Installer generator currently under development.");
+console.log("Please check back for future releases.");
+process.exit(1);
